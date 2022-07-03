cf07c4529470a18fdfadbdf6345f03ef39c7ba6b
张晗 <1123352498@qq.com>
2022-07-03 14:47:39 +0800

    employee

---
+++ code/ssm_test/src/main/java/com/lsp/domain/Employee.java
@@ -0,0 +1,16 @@
+package com.lsp.domain;
+
+/**
+ * @Author:
+ * @createTime: 2022年07月03日 14:00:41
+ * @version: 1.0
+ * @Description:
+ */
+public class Employee {
+    private Integer employee_id;
+    private String employee_name;
+    private String employee_gender;
+    private String employee_id_card;
+    private String employee_tel;
+    private String employee_address;
+}
