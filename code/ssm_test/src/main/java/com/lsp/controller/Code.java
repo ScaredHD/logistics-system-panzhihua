4314063b09031e9956811080218465e2bb15a0ac
qingfusheng <61900087+qingfusheng@users.noreply.github.com>
2022-07-07 09:20:23 +0800

    update_the_statuss_code

--- code/ssm_test/src/main/java/com/lsp/controller/Code.java
+++ code/ssm_test/src/main/java/com/lsp/controller/Code.java
@@ -7,16 +7,28 @@ package com.lsp.controller;
  * @Description:
  */
 public class Code {
-    public static final Integer SAVE_OK = 10011;
-    public static final Integer UPDATE_OK = 10021;
-    public static final Integer DELETE_OK = 10031;
-    public static final Integer GET_OK = 10041;
+    public static final Integer SAVE_OK = 200;
+    public static final Integer UPDATE_OK = 200;
+    public static final Integer DELETE_OK = 200;
+    public static final Integer GET_OK = 200;
 
-    public static final Integer SAVE_ERR = 10010;
-    public static final Integer UPDATE_ERR = 10020;
-    public static final Integer DELETE_ERR = 10030;
-    public static final Integer GET_ERR = 10040;
+    public static final Integer SAVE_ERR = 500;
+    public static final Integer UPDATE_ERR = 500;
+    public static final Integer DELETE_ERR = 500;
+    public static final Integer GET_ERR = 500;
 
-    public static final Integer STORE_ERR = 10050;
-    public static final Integer WITHDRAW_ERR = 10060;
+    public static final Integer STORE_ERR = 500;
+    public static final Integer WITHDRAW_ERR = 500;
+//    public static final Integer SAVE_OK = 10011;
+//    public static final Integer UPDATE_OK = 10021;
+//    public static final Integer DELETE_OK = 10031;
+//    public static final Integer GET_OK = 10041;
+//
+//    public static final Integer SAVE_ERR = 10010;
+//    public static final Integer UPDATE_ERR = 10020;
+//    public static final Integer DELETE_ERR = 10030;
+//    public static final Integer GET_ERR = 10040;
+//
+//    public static final Integer STORE_ERR = 10050;
+//    public static final Integer WITHDRAW_ERR = 10060;
 }
