7a948cd1bf17351da6de07ffeed6832fa4a05cf0
LiJianping <1196934490@qq.com>
2022-07-08 17:57:52 +0800

    修改登录界面

--- code/WebPage/src/views/login/login.vue
+++ code/WebPage/src/views/login/login.vue
@@ -2,122 +2,56 @@
   <div>
     <h1>登录</h1>
     <form class="form-group">
-      <input
-        v-model="username"
-        type="text"
-        placeholder="用户名"
-        required
-      />
-      <br/>
-      <input
-        v-model="passwordLogin"
-        type="password"
-        placeholder="密码"
-        required
-      />
+      <input v-model="username" type="text" placeholder="用户名" required />
+      <br />
+      <input v-model="password" type="password" placeholder="密码" required />
       <p>
         没有账号?
         <router-link to="/register">注册</router-link>
       </p>
-      <p><a href="#">忘记密码?</a></p>
+      <p><a href="#" @click="forgetPassword()">忘记密码?</a></p>
       <input type="submit" class="btn btn-primary" @click="doLogin" />
     </form>
   </div>
 </template>
 
 <script>
+import service from "@/utils/request";
+
+function Login() {
+  return service({
+    url: "/login",
+    method: "post",
+  });
+}
+
 export default {
   name: "login",
-  data: {
-    username: "",
-    password: ""
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
   },
 
   methods: {
     doLogin() {
-      if (this.username === "" || this.passwordLogin === "") {
-        this.emptyFields = true;
+      if (this.username === "" || this.password === "") {
+        alert("用户名或密码不能为空");
       } else {
-        alert("You are now logged in");
+        Login().then((res) => {
+          console.log(res.code);
+          console.log(res.msg);
+        });
       }
-    }
+    },
+
+    forgetPassword() {
+      alert("请联系管理员");
+    },
   },
 };
 </script>
 
 <style>
-/* p {
-  line-height: 1rem;
-}
-
-.card {
-  padding: 20px;
-}
-
-.form-group {
-  input {
-    margin-bottom: 20px;
-  }
-}
-
-.login-page {
-  align-items: center;
-  display: flex;
-  height: 100vh;
-
-  .wallpaper-login {
-    background: url(https://images.pexels.com/photos/32237/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)
-      no-repeat center center;
-    background-size: cover;
-    height: 100%;
-    position: absolute;
-    width: 100%;
-  }
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
-
-  .wallpaper-register {
-    background: url(https://images.pexels.com/photos/533671/pexels-photo-533671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)
-      no-repeat center center;
-    background-size: cover;
-    height: 100%;
-    position: absolute;
-    width: 100%;
-    z-index: -1;
-  }
-
-  h1 {
-    margin-bottom: 1.5rem;
-  }
-}
-
-.error {
-  animation-name: errorShake;
-  animation-duration: 0.3s;
-}
-
-@keyframes errorShake {
-  0% {
-    transform: translateX(-25px);
-  }
-  25% {
-    transform: translateX(25px);
-  }
-  50% {
-    transform: translateX(-25px);
-  }
-  75% {
-    transform: translateX(25px);
-  }
-  100% {
-    transform: translateX(0);
-  }
-} */
 </style>
