e7ecedcb95b9ce8ad8fb55b8cc7b9cf6754cf9fc
张晗 <1123352498@qq.com>
2022-07-09 17:29:59 +0800

    公司删除更新

--- code/ssm_test/src/main/java/com/lsp/controller/CompanyController.java
+++ code/ssm_test/src/main/java/com/lsp/controller/CompanyController.java
@@ -29,10 +29,10 @@ public class CompanyController {
         boolean flag = companyService.UpdateCompanyInfo(company);
         return new Result(flag?Code.UPDATE_OK:Code.UPDATE_ERR,flag);
     }
-    @DeleteMapping("/{company_id}")
+    @PutMapping ("/{company_id}")
     public Result DeleteCompanyInfo(@PathVariable Integer company_id) {
         boolean flag = companyService.DeleteCompanyInfo(company_id);
-        return new Result(flag?Code.DELETE_OK:Code.DELETE_ERR,flag);
+        return new Result(flag?Code.UPDATE_OK:Code.UPDATE_ERR,flag);
     }
     @GetMapping("/{search}")
     public Result SearchByCompanyName(@PathVariable String search) {
@@ -41,13 +41,6 @@ public class CompanyController {
         String msg = companyList != null ? "":"GET ERROR:PLEASE RETRY!";
         return new Result(code,companyList,msg);
     }
-//    @GetMapping("/{company_id}")
-//    public Result GetById(@PathVariable Integer company_id) {
-//        Company company = companyService.GetById(company_id);
-//        Integer code = company != null ? Code.GET_OK : Code.GET_ERR;
-//        String msg = company != null ? "":"GET ERROR:PLEASE RETRY!";
-//        return new Result(code,company_id,msg);
-//    }
     @GetMapping
     public Result DisplayCompanyInfo() {
         List<Company> companyList = companyService.DisplayCompanyInfo();
--- code/ssm_test/src/main/java/com/lsp/dao/CompanyDao.java
+++ code/ssm_test/src/main/java/com/lsp/dao/CompanyDao.java
@@ -11,19 +11,19 @@ import org.apache.ibatis.annotations.Update;
 import java.util.List;
 
 public interface CompanyDao {
-    @Insert("insert into company (company_name,company_tel,company_contacts) " +
-            "values(#{company_name},#{company_tel},#{company_contacts})")
+    @Insert("insert into company (company_name,company_tel,company_contacts,company_isvisible) " +
+            "values(#{company_name},#{company_tel},#{company_contacts},company_isvisible)")
     public void save(Company company);
-    @Update("update company set company_name = #{company_name},company_tel = #{company_tel},company_contacts = #{company_contacts} " +
-            "where company_id = #{company_id}")
+    @Update("update company set company_name = #{company_name},company_tel = #{company_tel},company_contacts = #{company_contacts}," +
+            "company_isvisible = #{company_isvisible} where company_id = #{company_id}")
     public void update(Company company);
-    @Delete("delete from company where company_id = #{company_id}")
+    @Update("update company set company_isvisible = '0' where company_id = #{company_id}")
     public void delete(Integer company_id);
     @Select("select company_id,company_name,company_tel,company_contacts from company " +
             "where company_name like concat('%',#{search},'%')")
     public List<Company> GetByName(String search);
-    @Select("select * from company where company_id = #{company_id}")
+    @Select("select * from company where company_id = #{company_id} and company_isvisible = '1'")
     public Company getById(Integer company_id);
-    @Select("select * from company")
+    @Select("select * from company where company_isvisible = '1'")
     public List<Company> getAll();
 }
